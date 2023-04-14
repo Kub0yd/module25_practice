37a706e663c0e1b845955f9dadc54a72c8580a96
Kub0yd <vs_egorov@mail.ru>
2023-04-14 17:29:48 +0300

    testing

--- test.php
+++ test.php
@@ -1,11 +1,9 @@
 <?php
    include "db_conf.php"; 
-   $userIDTemp = $db->prepare("SELECT id FROM users WHERE login = :login");
-    $userIDTemp->bindParam(':login, $login');
    $login = "Test1";
-   //получаем id авторизованного пользователя
-   $stmt = $userIDTemp->execute();
-   //$result= $stmt->FETCH(PDO::FETCH_ASSOC);
-   $userID = $stmt->fetchColumn();
-   echo $userId;
+   $login = $db->quote($login);
+   $sql = "SELECT id FROM users WHERE login = $login";
+   $userIDTemp = $db->query($sql);
+   $userID = $userIDTemp->fetchColumn(0);
+   echo $userID;
 ?>
